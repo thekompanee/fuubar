7715a6a365cee4a0cd055f2f509500e10bae54a9
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-11-15 12:06:21 +0100

    Add example_passed, example_pending, example_failed and state

--- lib/fuubar.rb
+++ lib/fuubar.rb
@@ -9,4 +9,25 @@ class Fuubar < RSpec::Core::Formatters::BaseTextFormatter
     @progress_bar = ProgressBar.new("#{example_count} examples", example_count, output)
   end
 
+  def example_passed(example)
+    super
+    increment
+  end
+
+  def example_pending(example)
+    super
+    @state = :yellow unless @state == :red
+    increment
+  end
+
+  def example_failed(example)
+    super
+    @state = :red
+    increment
+  end
+
+  def state
+    @state ||= :green
+  end
+
 end
--- spec/fuubar_spec.rb
+++ spec/fuubar_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Fuubar do
+
   before do
     @output = StringIO.new
     @formatter = Fuubar.new(@output)
@@ -34,5 +35,57 @@ describe Fuubar do
     it 'should set the finished_count to 0' do
       @formatter.instance_variable_get(:@finished_count).should == 0
     end
+
   end
+
+  describe 'passed, pending and failed' do
+    before do
+      @formatter.stub!(:increment)
+    end
+
+    describe 'example_passed' do
+
+      it 'should call the increment method' do
+        @formatter.should_receive :increment
+        @formatter.example_passed(@example)
+      end
+
+    end
+
+    describe 'example_pending' do
+
+      it 'should call the increment method' do
+        @formatter.should_receive :increment
+        @formatter.example_pending(@example)
+      end
+
+      it 'should set the state to :yellow' do
+        @formatter.example_pending(@example)
+        @formatter.state.should == :yellow
+      end
+
+      it 'should not set the state to :yellow when it is :red already' do
+        @formatter.instance_variable_set(:@state, :red)
+        @formatter.example_pending(@example)
+        @formatter.state.should == :red
+      end
+
+    end
+
+    describe 'example_failed' do
+
+      it 'should call the increment method' do
+        @formatter.should_receive :increment
+        @formatter.example_failed(@example)
+      end
+
+      it 'should set the state to :red' do
+        @formatter.example_failed(@example)
+        @formatter.state.should == :red
+      end
+
+    end
+
+  end
+
 end
