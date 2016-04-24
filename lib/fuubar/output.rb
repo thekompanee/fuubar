62703f2a4c7b9ac5bb3b766e5ae9309417b6ec6e
Jeff Felchner <accounts+git@thekompanee.com>
2016-04-24 02:30:09 -0500

    Added: Output module
    --------------------------------------------------------------------------------
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

---
+++ lib/fuubar/output.rb
@@ -0,0 +1,17 @@
+class Fuubar < RSpec::Core::Formatters::BaseTextFormatter
+class Output < Delegator
+  # :reek:BooleanParameter disable
+  def initialize(output, force_tty = false)
+    @raw_output = output
+    @force_tty  = force_tty
+  end
+
+  def __getobj__
+    @raw_output
+  end
+
+  def tty?
+    @force_tty || @raw_output.tty?
+  end
+end
+end
