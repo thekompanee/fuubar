26ab2e66d0644a157d5329d127ab9cb88f6a98ff
Jeff Felchner <catchall+git@jefffelchner.com>
2020-12-27 11:06:12 -0600

    Chore: Move Spec Files To lib Directory

--- spec/lib/fuubar/output_spec.rb
+++ spec/lib/fuubar/output_spec.rb
@@ -18,17 +18,17 @@ class TestTtyOutputClass
   end
 end
 
-class    Fuubar < RSpec::Core::Formatters::BaseTextFormatter
+class    Fuubar < ::RSpec::Core::Formatters::BaseTextFormatter
 describe Output do
   it 'delegates anything to the passed in object' do
-    output = Output.new(TestTtyOutputClass.new)
+    output = Output.new(::TestTtyOutputClass.new)
 
     expect(output.hello).to eql 'hello'
     expect(output).to       be_tty
   end
 
   it 'can override the TTY of the passed in class' do
-    output = Output.new(TestNonTtyOutputClass.new, true)
+    output = Output.new(::TestNonTtyOutputClass.new, true)
 
     expect(output).to be_tty
   end
